8b444b792b2200a3a1b1d83a881f60f6ed8e5704
vickykons <bikakikon96@gmail.com>
2017-11-13 18:45:53 +0200

    main_activity(java)

--- app/src/main/java/com/example/spiros/myapplication/MainActivity.java
+++ app/src/main/java/com/example/spiros/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//main_activity
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
 
@@ -22,5 +23,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         },SPLASH_TIME_OUT);
+
     }
 }
